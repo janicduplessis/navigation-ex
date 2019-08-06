95c1eaba5cc0bc61f226a5743e8bb90088126d24
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-08-06 23:57:54 +0200

    feat: add bottom tabs integration (#45)

---
+++ packages/bottom-tabs/src/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Navigators
+ */
+export { default } from './navigators/createBottomTabNavigator';
+
+/**
+ * Views
+ */
+export { default as BottomTabBar } from './views/BottomTabBar';
+
+/**
+ * Types
+ */
+export { BottomTabNavigationOptions, BottomTabNavigationProp } from './types';
